ac57bc7ab69c906cd2e31e5d727ff5f973402241
clmokaya <mokmakori@gmail.com>
2025-03-04 22:04:05 +0300

    tc 2

--- lib/themes/light_mode.dart
+++ lib/themes/light_mode.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
-    colorScheme: ColorScheme.dark(
+    colorScheme: ColorScheme.light(
         background: Colors.grey.shade300,
         primary: Colors.grey.shade200,
         secondary: Colors.white,
