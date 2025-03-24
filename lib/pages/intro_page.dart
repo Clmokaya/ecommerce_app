26314d6386e6201e846915d0fe4299779f83e722
clmokaya <mokmakori@gmail.com>
2025-03-24 21:45:02 +0300

    fonts

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -25,7 +25,7 @@ class IntroPage extends StatelessWidget {
             //title
             Text(
               'Minimal Shop',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 19),
             ),
             const SizedBox(
               height: 10,
